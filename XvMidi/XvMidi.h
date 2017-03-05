3b1e8530a988175c09c7d216436850f9b16572c3
jasonjsnell <snell.jason@gmail>
2017-03-05 11:41:02 +0100

    Initial Commit

---
+++ XvMidi/XvMidi.h
@@ -0,0 +1,19 @@
+//
+//  XvMidi.h
+//  XvMidi
+//
+//  Created by Jason Snell on 3/5/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvMidi.
+FOUNDATION_EXPORT double XvMidiVersionNumber;
+
+//! Project version string for XvMidi.
+FOUNDATION_EXPORT const unsigned char XvMidiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvMidi/PublicHeader.h>
+
+
